cfa8bf3f79645ef770aab44e8c9228bb61a7154b
Teh|Striker <kristianhanus@gmail.com>
2023-03-30 22:08:56 +0200

    Add liquibase

--- src/main/java/com/bohemian/app/entity/ItemDAO.java
+++ src/main/java/com/bohemian/app/entity/ItemDAO.java
@@ -15,8 +15,8 @@ import java.util.Date;
 public class ItemDAO {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "items_generator")
-    @SequenceGenerator(name = "items_generator", sequenceName = "item_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "item_seq")
+    @SequenceGenerator(name = "item_seq", sequenceName = "item_seq", allocationSize = 1)
     @JsonIgnore
     private Long id;
 
